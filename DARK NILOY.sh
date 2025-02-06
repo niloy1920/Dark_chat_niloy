e4c9e966fff66ad4d1c3a84b3d87623dd7cdc029
niloy1920 <niloymr4000@gmail.com>
2025-02-07 00:37:15 +0600

    Dark niloy
    Welcome

---
+++ DARK NILOY.sh
@@ -0,0 +1,89 @@
+!/bash/bin
+
+pkg install figlet -y
+
+figlet DARK NILOY
+
+apt update -y
+
+apt upgrade  -y
+
+pkg upgrade  -y
+
+termux-setup-storage
+
+pkg install git -y
+
+pkg install cmatrix
+
+cmatrix 
+
+
+apt list
+
+pkg install bash
+
+
+
+pkg install openssh -y
+
+pkg install php -y
+
+pkg install screenfetch
+
+screenfetch
+
+pkg install c -y
+
+pkg install pip -y
+
+apt install samba -y
+
+pkg install python -y
+
+pkg install python 2 -y
+
+pkg install cat -y
+
+pkg install clang -y
+
+pkg install nano -y
+
+pkg install rubi -y
+
+pkg install wget -y
+
+pkg install sl -y
+
+sl
+
+pkg install c script -y
+
+pkg install node.js -y
+
+pkg install curl -y
+
+pkg install perl -y
+
+pkg install apache 2 -y
+
+pkg install nmap -y
+
+figlet COMPLITE
+
+figlet DARK NILOY✓
+
+
+ls
+
+clear
+
+figlet auto remove niloy tools 
+
+pkg reinstall git
+
+rm -rf DARK NILOY.sh
+
+
+
+
